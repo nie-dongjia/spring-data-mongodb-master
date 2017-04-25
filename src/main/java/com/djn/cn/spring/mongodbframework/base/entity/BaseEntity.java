b52cbbd37ff17f03156db2f714d35ee21248c88c
聂冬佳 <niedongjiamail@qq.com>
2017-04-25 13:45:29 +0800

    实体添加

--- src/main/java/com/djn/cn/spring/mongodbframework/base/entity/BaseEntity.java
+++ src/main/java/com/djn/cn/spring/mongodbframework/base/entity/BaseEntity.java
@@ -1,5 +1,42 @@
 package com.djn.cn.spring.mongodbframework.base.entity;
 
-public class BaseEntity {
+import java.util.Date;
 
+/**
+ * 
+ * @ClassName BaseEntity
+ * @Description  基础封装实体
+ * @author NieDongjia
+ * @date 2017年4月25日 上午11:13:10
+ *
+ */
+public class BaseEntity {
+	private String id;
+	private String name;
+	private Date   createTime;
+	private Date   updateTime;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 }
---
+++ src/main/java/com/djn/cn/spring/mongodbframework/base/entity/UserInfo.java
@@ -0,0 +1,12 @@
+package com.djn.cn.spring.mongodbframework.base.entity;
+/**
+ * 
+ * @ClassName UserInfo
+ * @Description  用户
+ * @author NieDongjia
+ * @date 2017年4月25日 上午11:11:53
+ *
+ */
+public class UserInfo {
+
+}
